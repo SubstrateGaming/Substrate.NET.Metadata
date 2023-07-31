0ac2a57de2ea35f356c6b2e8acd1dc98a177cc35
Apolixit <friotromain@gmail.com>
2023-07-31 17:14:13 +0200

    Add shared interface for differential modules

---
+++ Substrate.NET.Metadata/Compare/Base/IMetadataDiff.cs
@@ -0,0 +1,23 @@
+﻿using Substrate.NET.Metadata.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substrate.NET.Metadata.Compare.Base
+{
+    public interface IMetadataDiffBase<out T>
+        where T : IMetadataDifferentialModules
+    {
+        public IEnumerable<T> AllModulesDiff { get; }
+
+        public IEnumerable<T> UnchangedModules { get; }
+
+        public IEnumerable<T> ChangedModules { get; }
+
+        public IEnumerable<T> AddedModules { get; }
+
+        public IEnumerable<T> RemovedModules { get; }
+    }
+}
